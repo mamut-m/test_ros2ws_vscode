f26f3dc3ff28294db7168101f4d906d0a65a70f1
Markus Murschitz <markus.murschitz@ait.ac.at>
2023-12-17 17:20:49 +0000

    fixing stupid warngins during testing again

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-set -e
-
 if [ -f install/setup.bash ]; then source install/setup.bash; fi
-colcon test --merge-install
-colcon test-result --verbose
+set -x
+
+# to avoid topics/pointcloud_publisher/test/test_flake8.py::test_flake8
+#   Warning: SelectableGroups dict interface is deprecated. Use select.
+#       -- Docs: https://docs.pytest.org/en/stable/warnings.html
+PYTHONWARNINGS="ignore: SelectableGroups" \
+        colcon test --merge-install 
+        #--packages-select examples_rclpy_pointcloud_publisher
+        colcon test-result --verbose
+
