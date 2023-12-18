b2e6fb06cc894efbf511e4f39adab5b6f0dfaa33
Markus Murschitz <markus.murschitz@ait.ac.at>
2023-12-18 15:01:42 +0000

    fixed ros version in bashrc

--- .devcontainer/post_create_command.sh
+++ .devcontainer/post_create_command.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xeu
 
-echo source /opt/ros/humble/setup.bash >> /home/ros/.bashrc
+echo source /opt/ros/$ROS_DISTRO/setup.bash >> /home/ros/.bashrc
 echo "if [ -f /workspace/install/setup.bash ]; then source /workspace/install/setup.bash; fi" >> /home/ros/.bashrc
